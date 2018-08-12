72a1bc5d8bb82a7e84be0b22e97a9b09343c2718
Dan Munckton <dan@cultivatehq.com>
2018-08-12 23:37:48 +0100

    Add call_a_function assembly files

--- call_a_function.simplified.s
+++ call_a_function.simplified.s
@@ -1,32 +1,22 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.macosx_version_min 10, 12
 	.globl	_main
-	.p2align	4, 0x90
 _main:                                  ## @main
 ## BB#0:
 	pushl	%ebp
 	movl	%esp, %ebp
-	subl	$24, %esp
-	movl	$17, %eax
-	movl	$0, -4(%ebp)
-	movl	$17, (%esp)
-	movl	%eax, -8(%ebp)          ## 4-byte Spill
+	pushl	$17
 	calll	_add_13
-	addl	$24, %esp
+	addl	$4, %esp
 	popl	%ebp
 	retl
 
-	.p2align	4, 0x90
 _add_13:                                ## @add_13
 ## BB#0:
 	pushl	%ebp
 	movl	%esp, %ebp
-	pushl	%eax
 	movl	8(%ebp), %eax
-	movl	%eax, -4(%ebp)
-	movl	-4(%ebp), %eax
 	addl	$13, %eax
-	addl	$4, %esp
 	popl	%ebp
 	retl
 
