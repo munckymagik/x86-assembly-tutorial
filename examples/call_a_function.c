29ff5bc0d217bda5005d3e57e255bf1a359b538b
Dan Munckton <munckfish@gmail.com>
2020-01-26 09:51:36 +0000

    WIP

--- examples/call_a_function.c
+++ examples/call_a_function.c
@@ -1,9 +1,11 @@
-static int add_13(int input) {
-    return input + 13;
+#include <limits.h>
+
+long __attribute__ ((noinline)) add_13(long input0, long input1, long input2, long input3, long input4, long input5) {
+    return input0 + input1 + input2 + input3 + input4 + input5 + 13;
 }
 
 int main() {
-    int result = add_13(17);
+    long result = add_13(1, 2, 3, 5, 8, 11);
 
-    return result;
+    return (int) result;
 }
